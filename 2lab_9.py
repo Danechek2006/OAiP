d07c73001d2a7eae152344d0c365bd21a863c694
Danechek2006 <145415459+Danechek2006@users.noreply.github.com>
2023-11-05 17:17:32 +0900

    Update 2lab_9.py

--- 2lab_9.py
+++ 2lab_9.py
@@ -1,5 +1,5 @@
-pozavchera = int(input("Введите чисто покупателей за позавчера:")) 
-vchera = int(input("Введите чисто покупателей за вчера:")) 
+pozavchera = int(input("Введите чисто покупателей за позавчера: ")) 
+vchera = int(input("Введите чисто покупателей за вчера: ")) 
 if pozavchera < vchera:
     print("Сегодня магазин посетит:" , vchera +(vchera - pozavchera), "человек")
 elif pozavchera > vchera:
